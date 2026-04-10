bb1058ea1a64a3b58dd84707ae562ee3e8681d52
pasttelstudios <pasttelstudios@gmail.com>
2026-04-10 17:24:23 -0500

    La Mejor

--- src/app/api/lookup/route.ts
+++ src/app/api/lookup/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../../../lib/auth";
+import { authOptions } from "@/lib/auth";
 
 export async function GET(req: Request) {
   const session = await getServerSession(authOptions);
